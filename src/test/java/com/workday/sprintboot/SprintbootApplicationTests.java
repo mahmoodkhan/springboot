75d21c3fe7ab916863a7b613bd19fc9c9d2d4d1b
Mahmood Khan <mahmood.khan@workday.com>
2025-10-03 21:25:03 -0700

    initial commit

---
+++ src/test/java/com/workday/sprintboot/SprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workday.sprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
